ecdfc16f94474e219bf8575df10c37d0e45b6eae
smit <smitpatel6988@gmail.com>
2026-02-27 14:21:42 +0530

    change code in multer middleware file

--- backend/src/middlewares/multer.middlewares.js
+++ backend/src/middlewares/multer.middlewares.js
@@ -1,14 +1,15 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-      cb(null,Date.now()+file.originalname)
-    }
-  })
-  
+  destination: function (req, file, cb) {
+    cb(null, "/tmp"); // ✅ Render writable directory
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
 export const upload = multer({
-    storage 
-})
+  storage,
+});
